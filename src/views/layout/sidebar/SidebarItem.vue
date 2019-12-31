33901c99f23ed460efe4fdf0a275e779ebabfa64
abu <294333196@qq.com>
2019-12-31 14:50:35 +0800

    增加菜单路由适配兼容

--- src/views/layout/sidebar/SidebarItem.vue
+++ src/views/layout/sidebar/SidebarItem.vue
@@ -16,16 +16,54 @@
         ></i>
         <span slot="title">
           {{
-          item.hasOwnProperty("meta") && item.meta.hasOwnProperty("title")
-          ? item.meta.title
-          : item.path + "未设置菜单名称"
+            item.hasOwnProperty("meta") && item.meta.hasOwnProperty("title")
+              ? item.meta.title
+              : item.path + "未设置菜单名称"
+          }}
+        </span>
+      </el-menu-item>
+
+      <!-- 此菜单下只有一个子菜单（直接指向该菜单） -->
+      <el-menu-item
+        v-if="
+          item.children &&
+            item.children.length == 1 &&
+            item.hasOwnProperty('hidden') &&
+            !item.hidden
+        "
+        :key="item.index"
+        :index="
+          parent
+            ? parent + '/' + item.path + '/' + item.children[0].path
+            : item.path + '/' + item.children[0].path
+        "
+      >
+        <i
+          :class="
+            item.children[0].hasOwnProperty('meta') &&
+            item.children[0].meta.hasOwnProperty('icon')
+              ? item.children[0].meta.icon
+              : ''
+          "
+        ></i>
+        <span slot="title">
+          {{
+            item.children[0].hasOwnProperty("meta") &&
+            item.children[0].meta.hasOwnProperty("title")
+              ? item.children[0].meta.title
+              : item.children[0].path + "未设置菜单名称"
           }}
         </span>
       </el-menu-item>
 
       <!-- 此菜单下还有子菜单 -->
       <el-submenu
-        v-if="item.children && item.hasOwnProperty('hidden') && !item.hidden"
+        v-if="
+          item.children &&
+            item.children.length > 1 &&
+            item.hasOwnProperty('hidden') &&
+            !item.hidden
+        "
         :key="item.index"
         :index="parent ? parent + '/' + item.path : item.path"
       >
@@ -39,9 +77,9 @@
           ></i>
           <span>
             {{
-            item.hasOwnProperty("meta") && item.meta.hasOwnProperty("title")
-            ? item.meta.title
-            : item.path + "未设置菜单名称"
+              item.hasOwnProperty("meta") && item.meta.hasOwnProperty("title")
+                ? item.meta.title
+                : item.path + "未设置菜单名称"
             }}
           </span>
         </template>
@@ -57,12 +95,12 @@
 
 <script>
 export default {
-  name: 'SidebarItem',
-  props: ['menu', 'parent'],
+  name: "SidebarItem",
+  props: ["menu", "parent"],
   data() {
-    return {}
+    return {};
   }
-}
+};
 </script>
 
 <style scoped>
